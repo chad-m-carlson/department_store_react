fd6c1b4e7c5fb803edb3a91a0a8ab9e54d9c6b60
Chad Carlson <chad.m.carlson@gmail.com>
2019-06-12 13:33:45 -0600

    items load with hook

--- client/src/components/ItemIndex.js
+++ client/src/components/ItemIndex.js
@@ -1,47 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Card, } from 'semantic-ui-react';
 import {Link, } from 'react-router-dom';
 import axios from 'axios';
 
-class ItemIndex extends React.Component {
-    state = {
-      items: [{}], 
-    }
+// class ItemIndex extends React.Component {
+//     state = {
+//       items: [{ }],
+//     }
 
-  componentDidMount(){
-  const {dId} = this.props
-  axios.get(`/api/departments/${dId}/items`)
-  .then( res=> {
-    this.setState({items: res.data});
-  })}
+const ItemIndex = (props) => {
+  const [items, setItems] = useState([])
+  
 
   
-renderItems = () => {
-  const {items, } = this.state
-  if (items.length <= 0) return <h2>No Departments</h2>
-  return items.map(i => (
+  useEffect(() => {
+    axios.get(`/api/departments/${props.dId}/items`)
+    .then( res => {setItems(res.data)})}, [props])
+  
+  
+  const renderItems = () => {
+    // const {items, } = this.state
+    if (items.length <= 0) return <h2>No Items</h2>
+    return items.map(i => (
     <Link 
-    to={`/departments/1/items/${i.id}`}
+    to={`/departments/${props.dId}/items/${i.id}`}
     key={i.id}
     >
-    <Card style={{padding: '10px', margin: '20px'}}>
-      <Card.Content>
-        <Card.Header>{i.name}</Card.Header>
-        <Card.Description>{i.description}</Card.Description>
-      </Card.Content>
-    </Card>
+      <Card style={{padding: '10px', margin: '20px'}}>
+        <Card.Content>
+          <Card.Header>{i.name}</Card.Header>
+          <Card.Description>{i.description}</Card.Description>
+        </Card.Content>
+      </Card>
     </Link>
     ));
   };
-
-  render() {
+  console.log(props)
     return(
       <>
     <h1>ItemIndex</h1>
-    {this.renderItems()}
+    {renderItems()}
     </>
   );
-}
 }
 
 export default ItemIndex;
